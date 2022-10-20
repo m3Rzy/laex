5fd6a358717b0ad6e6e63f5aab147a7a18aedaaa
hoiboi <gentledemeiz@gmail.com>
2022-10-21 02:30:18 +0300

    card done

--- laex_front/src/pages/otech.js
+++ laex_front/src/pages/otech.js
@@ -3,6 +3,11 @@ import { FaAngleDown, FaAngleLeft } from "react-icons/fa";
 import { AiOutlineBell } from "react-icons/ai";
 import { IoChatbubblesOutline } from "react-icons/io5";
 import { FaAngleRight } from "react-icons/fa";
+import { AiOutlineCalendar } from "react-icons/ai"
+import { VscError } from "react-icons/vsc"
+import { AiOutlineHeart } from "react-icons/ai"
+import { BsChatDots } from "react-icons/bs"
+import { FaTelegramPlane } from "react-icons/fa"
 
 function Otech() {
   return (
@@ -41,17 +46,51 @@ function Otech() {
           <h1 className="hooks__pag__custom">John Deere 9660STS</h1>
         </div>
         <h1 className="title__page">Комбайн John Deere 9660STS (ID000009)</h1>
-        <div className="order__container__pic">
-          {/* <div className="picture" /> */}
-
-          <div className="left__right">
-            <div className="angle__left__page">
-              <FaAngleLeft />
-            </div>
-            <div className="angle__right__page">
-              <FaAngleRight />
+        {/* Нужен контейнер большой для 2ух */}
+        <div className="container">
+          <div className="order__container__pic">
+            {/* <div className="picture" /> */}
+            <div className="left__right">
+              <div className="angle__left__page">
+                <FaAngleLeft />
+              </div>
+              <div className="angle__right__page">
+                <FaAngleRight />
+              </div>
             </div>
           </div>
+          <div className="right__container">
+            <h1 className="card__title">Александр Александров</h1>
+            <div className="card__subs">
+              <AiOutlineCalendar />
+              <p className="card__subs__p">На сайте с 11.11.2021</p>
+            </div>
+            <div className="card__status__container">
+              <p className="card__status_p">Статус</p>
+              <div className="card__status">Свободна</div>
+            </div>
+            <div className="card__status__container">
+              <p className="card__status_p">Верификация</p>
+              <div className="card__status">Верифицирован</div>
+            </div>
+            <a className="card__button__report" href="#">
+              <VscError className="vsc__error" size={19} />
+              <a href="#" className="card__button__a">Пожаловаться</a>
+            </a>
+            <a className="card__button__fav" href="#">
+              <AiOutlineHeart className="heart" size={19} />
+              <a href="#" className="card__button__a__fav">Добавить в избранное</a>
+            </a>
+            <a className="card__button__chat" href="#">
+              <BsChatDots className="chat" size={19} />
+              <a href="#" className="card__button__a__chat">Начать чат</a>
+            </a>
+            <a className="card__button__deal" href="#">
+              <FaTelegramPlane className="deal" size={19} />
+              <a href="#" className="card__button__a__deal">Предложить сделку</a>
+            </a>
+          </div>
+          <div className="card__container"></div>
         </div>
       </div>
     </div>
